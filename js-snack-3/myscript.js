ec0846f2342981229237a95fa1dce20d0de19189
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-06 18:14:51 +0100

    Correzione

--- js-snack-3/myscript.js
+++ js-snack-3/myscript.js
@@ -1,6 +1,13 @@
+/**
+ * 
+Il software deve chiedere per 10 volte all'utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+ */
+
+/* Variabile flag che userò nel ciclo per sommare i 10 numeri richiesti all'utente */
 let sum = 0;
 
-for (i = 0; i < 11; i++) {
+for (i = 0; i < 10; i++) {
     let userNumber = parseInt(prompt('scegli un numero'));
 
     console.log(userNumber); 
@@ -8,5 +15,7 @@ for (i = 0; i < 11; i++) {
     sum += userNumber;
 
     console.log(sum);
+
+
 }
 
