39bf81852654399a83365d1d896a04e07d71dd9f
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-06 18:41:53 +0100

    Correzione

--- js-snack-9/js/myscript.js
+++ js-snack-9/js/myscript.js
@@ -1,7 +1,26 @@
+/**
+ *
+ *
+    Crea un array vuoto e chiedi all'utente un numero da inserire nell'array.
+    Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 200.
+ *
+ */
+
+let array = [];
+
 let somma = 0;
 
-while (somma < 200) {
-    somma = somma + parseInt(prompt('Scegli un numero'));
+let indice = 0;
 
-    console.log(somma);
+while (somma < 200) {
+    array.push(parseInt(prompt("Scegli un numero")));
+
+    somma += array[array.length - 1];
+    indice++;
 } 
+
+console.log(array);
+
+console.log(somma);
+
+console.log(indice);
