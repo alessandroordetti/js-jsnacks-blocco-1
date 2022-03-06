24226ad8755ba5e7a3585c80dd13637130fe70f1
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-06 18:53:51 +0100

    Correzione

--- js-snack-10/myscript.js
+++ js-snack-10/myscript.js
@@ -1,3 +1,10 @@
+/**
+    Fai inserire un numero, che chiameremo N, all'utente.
+    Genera N array, ognuno formato da 10 numeri casuali tra 4 e 4532.
+    Ogni volta che ne crei uno, stampalo a schermo.
+ */
+
+
 let N = parseInt(prompt('scegli un numero'));
 console.log(N);
 
@@ -12,10 +19,12 @@ if (N > 10) {
 let arrays = [];
 
 for (let i = 0; i < N; i++) {
-    const array = [];
-    while (array.length < 10) {
-        array.push(Math.floor(Math.random() * 4352) + 4);
+    const singleArray = [];
+    while (singleArray.length < 10) {
+        singleArray.push(Math.floor(Math.random() * 4352) + 4);
     }
 
-    console.log(array);
+    console.log(singleArray);
+
+    arrays.push(singleArray);
 }
