9e1ff19a9fc0e5b503854d292696910dba006eb1
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-06 17:54:16 +0100

    modifica

--- js-snack-2/myscript.js
+++ js-snack-2/myscript.js
@@ -3,11 +3,12 @@ let firstWord = prompt('Scegli una parola');
 let secondWord = prompt('Scegli una seconda parola'); 
 
 if (firstWord.length > secondWord.length){
-    console.log (secondWord);
     console.log (firstWord);
+    console.log (secondWord);
+    
 } else if (firstWord.length < secondWord.length){
-    console.log (firstWord);
     console.log (secondWord);
+    console.log (firstWord);
 } else {
     console.log ('Le parole hanno la stessa lunghezza');
 }
