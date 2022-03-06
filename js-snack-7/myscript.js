a1bf1c288167b161c7f16f444f321f74ae97eea7
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-06 18:25:27 +0100

    Correzione

--- js-snack-7/myscript.js
+++ js-snack-7/myscript.js
@@ -1,13 +1,20 @@
-const arrayLenght = 10;
+/**
+ * Creiamo un array con dieci numeri randomici
+ * calcoliamo la somma di tutti gli elementi
+ * e calcoliamo anche il valore medio
+ */
+
 
 const randomArray = [];
 
 let sum = 0
 
-for (let i = 0; i < arrayLenght; i++) {
-    randomArray.push(Math.floor(Math.random() * 100));
-    sum = randomArray / 10;
+for (let i = 0; i < 10; i++) {
+    let randomNumber = (Math.floor(Math.random() * 100));
+    randomArray.push(randomNumber);
+    sum += randomArray[i];
 }
 
 console.log(randomArray);
 console.log(sum);
+console.log(sum / randomArray.length);
